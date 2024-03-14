3b09996d4cfb6d9afbb4bc17b62eccc380f92f00
James Fitzgerald <james@jawfish.dev>
2024-03-14 14:19:25 -0400

    add errors for cache validation

--- err.go
+++ err.go
@@ -7,7 +7,7 @@ type ErrUnsupportedArch struct {
 }
 
 func (e *ErrUnsupportedArch) Error() string {
-	return fmt.Sprintf("Architecture %s not supported", e.Arch)
+	return fmt.Sprintf("architecture %s not supported", e.Arch)
 }
 
 type ErrMissingDep struct {
@@ -22,5 +22,47 @@ type ErrPackageManagerNotFound struct {
 }
 
 func (e *ErrPackageManagerNotFound) Error() string {
-	return "No package manager found in PATH. This program requires dnf or dnf5 to be installed and in the PATH."
+	return "no package manager found in PATH"
+}
+
+type ErrInvalidCacheSchema struct {
+	Table string
+}
+
+func (e *ErrInvalidCacheSchema) Error() string {
+	return fmt.Sprintf("invalid schema for table %s", e.Table)
+}
+
+type ErrQueryFailed struct {
+	Query string
+	Err   error
+}
+
+func (e *ErrQueryFailed) Error() string {
+	return fmt.Sprintf("query %s failed: %s", e.Query, e.Err)
+}
+
+type ErrScanFailed struct {
+	Table string
+	Err   error
+}
+
+func (e *ErrScanFailed) Error() string {
+	return fmt.Sprintf("scanning table %s failed: %s", e.Table, e.Err)
+}
+
+type ErrOpeningCacheDatabase struct {
+	Err error
+}
+
+func (e *ErrOpeningCacheDatabase) Error() string {
+	return fmt.Sprintf("error opening cache database: %s", e.Err)
+}
+
+type ErrCacheNotFound struct {
+	Path string
+}
+
+func (e *ErrCacheNotFound) Error() string {
+	return fmt.Sprintf("cache not found at path %s", e.Path)
 }
